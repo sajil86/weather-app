b4530012fcc2f4deca28b3f4c81c4369150f2178
Sajil G <sajilganga86@gmail.com>
2023-08-24 23:28:18 +0530

    Working
    Error Fixed

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -14,14 +14,26 @@ const WeatherApp = () => {
 
    let api_key = "051b702fab90229cf89e42c383eb47d1";
 
-   const search = () => {
+   const search = async () => {
       const element =document.getElementsByClassName("cityInput")
       if(element[0].value==="")
       {
         return 0;
       }
       let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+      
+      let response = await fetch(url);
+      let data = await response.json();
+      
+      const humidity =document.getElementsByClassName("humidity-percent");
+      const wind = document.getElementsByClassName("wind-rate");
+      const temprature = document.getElementsByClassName("weather-temp");
+      const location = document.getElementsByClassName("weather-location");
 
+      humidity[0].innerHTML = data.main.humidity;
+      wind[0].innerHTML = data.wind.speed;
+      temprature[0].innerHTML = data.main.temp;
+      location[0].innerHTML = data.name;
    }
   return (
     <div className='container'>
@@ -48,7 +60,7 @@ const WeatherApp = () => {
       <div className="element">
             <img src={wind_icon} alt="" className="icon"/>
             <div className="data">
-                <div className="humidity-percent">18 km?h</div>
+                <div className="wind-rate">18 km?h</div>
                 <div className="text">Wind Speed</div>
             </div>
         </div>
