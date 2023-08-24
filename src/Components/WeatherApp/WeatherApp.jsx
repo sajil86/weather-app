1f0f8dfc8ef8f0f6faadaf83e394ba612b94817e
Sajil G <sajilganga86@gmail.com>
2023-08-24 23:13:56 +0530

    1.2

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,11 +11,23 @@ import wind_icon from "../Assets/wind.png";
 import humidity_icon from "../Assets/humidity.png";
 
 const WeatherApp = () => {
+
+   let api_key = "051b702fab90229cf89e42c383eb47d1";
+
+   const search = () => {
+      const element =document.getElementsByClassName("cityInput")
+      if(element[0].value==="")
+      {
+        return 0;
+      }
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+   }
   return (
     <div className='container'>
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder='Search' />
-        <div className="search-icon">
+        <div className="search-icon" onClick={()=>{search()}}>
             <img src={search_icon} alt=""/>
         </div>
       </div>
